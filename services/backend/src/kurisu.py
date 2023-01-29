018f056fa3123546d09800ef668f2c3a530e07b7
Mehrdad Heshmat <58226760+MR-EIGHT@users.noreply.github.com>
2023-01-29 06:21:48 +0330

    route problem solved.

--- services/backend/src/kurisu.py
+++ services/backend/src/kurisu.py
@@ -63,7 +63,7 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@app.get("/users/{user_name}", response_model=schemas.User)
+@app.get("/user/", response_model=schemas.User)
 def read_user_by_uname(user_name: str, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_username(db, username=user_name)
     if db_user is None:
